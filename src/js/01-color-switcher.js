704e6ebcf6afe8733bf62eb0b1e71e07a36c69de
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-02-03 13:34:08 +0200

    complete first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+  prg: document.querySelector('p'),
+};
 
+const div = document.createElement('div');
+div.classList.add('boxForButton');
+refs.start.classList.add('button-start');
+refs.stop.classList.add('button-stop');
+div.append(refs.start, refs.stop);
+refs.prg.after(div);
+
+refs.stop.setAttribute('disabled', true);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.start.addEventListener('click', () => {
+  refs.start.setAttribute('disabled', true);
+  refs.stop.removeAttribute('disabled', false);
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+});
+
+refs.stop.addEventListener('click', () => {
+  refs.start.removeAttribute('disabled', false);
+  clearInterval(timerId);
+});
